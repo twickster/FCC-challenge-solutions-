022ecdda8da39912332b774abb7f13be839f3b10
twickster <twickster@users.noreply.github.com>
2017-04-18 12:35:38 +0100

    Create Repeat a string repeat a string.js
    Added solution to another exercise

---
+++ Repeat a string repeat a string.js
@@ -0,0 +1,23 @@
+*/
+Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+repeatStringNumTimes("*", 3) should return "***".
+repeatStringNumTimes("abc", 3) should return "abcabcabc".
+repeatStringNumTimes("abc", 4) should return "abcabcabcabc".
+repeatStringNumTimes("abc", 1) should return "abc".
+repeatStringNumTimes("*", 8) should return "********".
+repeatStringNumTimes("abc", -2) should return "".
+
+*/
+
+function repeatStringNumTimes(str, num) {
+  var answer = "";
+  
+  for ( var i = 0; i < num; i++){
+    answer += str;;  
+  }
+  
+  return answer;
+}
+
+repeatStringNumTimes("abc", 3);
